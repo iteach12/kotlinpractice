178884572768da171aa067442a72f1b4a584aca3
iteach12 <iteach12@gmail.com>
2018-08-27 21:33:14 +0900

    fff

---
+++ src/VariablePrac.kt
@@ -0,0 +1,5 @@
+val a: Int = 7
+val b = 10
+val c : String = "hello"
+val d = "metoo"
+
---
+++ src/practice0827.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+//    println(makeMessage4(1))
+//    println(makeMessage4(2))
+//    println(makeMessage5(3))
+//    println(makeMessage5(4))
+
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+
+}
+
+fun makeMessage4(msgType: Int) : String {
+    return if (msgType == 1) "1번입니다." else "1번이 아니군요"
+}
+
+fun makeMessage5(msgType: Int) = if (msgType == 3) "3번입니다." else "3번이 아니군요"
+
+
